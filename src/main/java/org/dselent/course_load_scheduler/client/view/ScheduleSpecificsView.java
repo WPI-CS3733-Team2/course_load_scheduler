c30eef13b1d245b62c140f4693f3230be9719ee6
lahigham <higham.laurel@gmail.com>
2018-02-14 13:40:59 -0500

    scheduleSpecifics, scheduleSearch, scheduleList, facultyCourseMapping

---
+++ src/main/java/org/dselent/course_load_scheduler/client/view/ScheduleSpecificsView.java
@@ -0,0 +1,23 @@
+package org.dselent.course_load_scheduler.client.view;
+
+import org.dselent.course_load_scheduler.client.presenter.ScheduleSpecificsPresenter;
+
+import com.google.gwt.user.cellview.client.DataGrid;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public interface ScheduleSpecificsView extends BaseView<ScheduleSpecificsPresenter>
+{
+	TextBox getReport();
+	void setReport(TextBox report);
+	Grid getCalendarGrid();
+	void setCalendarGrid(Grid calendarGrid);
+	VerticalPanel getVerticalPanel();
+	void setVerticalPanel(VerticalPanel verticalPanel);
+	ListBox getNavDropDown();
+	void setNavDropDown(ListBox navDropDown);
+	
+}
