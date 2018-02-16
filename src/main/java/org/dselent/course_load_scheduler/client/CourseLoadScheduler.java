7e4c75d71ad6416db23345528ba196f34d68099a
Jinwei Zhang <zhangjinwei786@gmail.com>
2018-02-15 23:08:04 -0500

    small change for requestInbox

--- src/main/java/org/dselent/course_load_scheduler/client/CourseLoadScheduler.java
+++ src/main/java/org/dselent/course_load_scheduler/client/CourseLoadScheduler.java
@@ -94,7 +94,7 @@ public class CourseLoadScheduler implements EntryPoint
 		indexPresenter.go(root);
 		adminCoursePresenter.go(indexView.getViewRootPanel());
 
-		switch (6) {
+		switch (8) {
 
 		case 0:
 			LoginPresenterImpl loginPresenter = injector.getLoginPresenter();
--- src/main/java/org/dselent/course_load_scheduler/client/view/impl/RequestInboxViewImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/view/impl/RequestInboxViewImpl.java
@@ -19,11 +19,6 @@ public class RequestInboxViewImpl extends BaseViewImpl<RequestInboxPresenter> im
 
 	private static RequestInboxViewImplUiBinder uiBinder = GWT.create(RequestInboxViewImplUiBinder.class);
 	@UiField VerticalPanel RequestInboxPanel;
-	@UiField Button AccountButton;
-	@UiField Button CourcesButton;
-	@UiField Button SchedulesButton;
-	@UiField Button UsersButton;
-	@UiField Button InboxButton;
 	@UiField Button ApproveButton;
 	@UiField Button DenyButton;
 	@UiField Button DeleteButton;
@@ -45,31 +40,6 @@ public class RequestInboxViewImpl extends BaseViewImpl<RequestInboxPresenter> im
 	}
 	
 	@Override
-	public Button getAccountButton() {
-		return AccountButton;
-	}
-	
-	@Override
-	public Button getCourcesButton() {
-		return CourcesButton;
-	}
-	
-	@Override
-	public Button getSchedulesButton() {
-		return SchedulesButton;
-	}
-	
-	@Override
-	public Button getUsersButton() {
-		return UsersButton;
-	}
-	
-	@Override
-	public Button getInboxButton() {
-		return InboxButton;
-	}
-	
-	@Override
 	public Button getApproveButton() {
 		return ApproveButton;
 	}
@@ -82,22 +52,6 @@ public class RequestInboxViewImpl extends BaseViewImpl<RequestInboxPresenter> im
 	@Override
 	public Button getDeleteButton() {
 		return DeleteButton;
-	}
-	
-	@UiHandler("AccountButton")
-	void onAccountButtonClick(ClickEvent event) {
-	}
-	@UiHandler("CourcesButton")
-	void onCourcesButtonClick(ClickEvent event) {
-	}
-	@UiHandler("SchedulesButton")
-	void onSchedulesButtonClick(ClickEvent event) {
-	}
-	@UiHandler("UsersButton")
-	void onUsersButtonClick(ClickEvent event) {
-	}
-	@UiHandler("InboxButton")
-	void onInboxButtonClick(ClickEvent event) {
 	}
 	@UiHandler("ApproveButton")
 	void onApproveButtonClick(ClickEvent event) {
